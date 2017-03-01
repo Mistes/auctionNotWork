c2e4807fc1741508d8bf13448e9aeda3329ff685
DemianKurilenko <kurilenko.dm@gmail.com>
2017-03-01 17:27:37 +0200

    Initial commit

---
+++ app/components/home/home.ts
@@ -0,0 +1,48 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Product, ProductService} from '../../services/product-service';
+import 'rxjs/add/operator/debounceTime';
+import {Observable} from "rxjs";
+
+@Component({
+  selector: 'auction-home-page',
+  styleUrls: ['app/components/home/home.css'],
+  template: `
+    <div class="row carousel-holder">
+      <div class="col-md-12">
+        <auction-carousel></auction-carousel>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="form-group">
+          <input placeholder="Filter products by title"
+                 class="form-control" type="text"
+                 [formControl]="titleFilter">
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div *ngFor="let product of products | async " class="col-sm-4 col-lg-4 col-md-4">
+        <auction-product-item [product]="product"></auction-product-item>
+      </div>
+    </div>
+  `
+})
+ export default class HomeComponent implements OnInit{
+  products: Observable<Product[]>;
+  titleFilter: FormControl = new FormControl();
+  filterCriteria: string;
+
+  constructor(private productService: ProductService) {}
+  ngOnInit(){
+    this.products = this.productService.getProducts();
+  }
+   /* this.products = this.productService.getProducts();
+    this.titleFilter.valueChanges
+      .debounceTime(100)
+      .subscribe(
+        value => this.filterCriteria = value,
+        error => console.error(error));
+  }*/
+}
